45dd3fb7407c71ed5a2747164319d73662370f85
Arifian <arifian.rahardianda@zeniuseducation.com>
2023-01-03 11:50:15 +0700

    proper form with method

--- netlify/functions/counter.ts
+++ netlify/functions/counter.ts
@@ -3,11 +3,24 @@ import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 let counter = 0;
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  counter++;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: counter }),
-  };
+  if (event.httpMethod === 'POST') {
+    counter++;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ count: counter }),
+    };
+  } else if (event.httpMethod === 'GET') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ count: counter }),
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed',
+    };
+  }
 };
+
 
 export { handler };
