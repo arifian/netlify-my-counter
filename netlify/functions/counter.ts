2138ff83390a9f4777315429c2a7af1caba81d40
Arifian <arifian.rahardianda@zeniuseducation.com>
2023-01-04 11:52:21 +0700

    added database, count correctly, init stil broken

--- netlify/functions/counter.ts
+++ netlify/functions/counter.ts
@@ -1,19 +1,40 @@
+import * as dotenv from 'dotenv';
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import * as mysql from 'mysql2';
 
-let counter = 0;
+// create the connection to database
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+let counter: number;
+
+// retrieve the count from the database
+connection.query('SELECT count FROM counters WHERE id = 1', function (error, results, fields) {
+  if (error) throw error;
+  counter = results[0].count;
+});
+
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   if (event.httpMethod === 'POST') {
     counter++;
+    // save the new counter value to the database
+    connection.query('UPDATE counters SET count = ? WHERE id = 1', [counter], function (error, results, fields) {
+      if (error) throw error;
+    });
+          // return the updated count to the client
     return {
-      statusCode: 200,
-      body: JSON.stringify({ count: counter }),
-    };
+        statusCode: 200,
+        body: JSON.stringify({ count: counter }),
+      };
   } else if (event.httpMethod === 'GET') {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ count: counter }),
-    };
+    // retrieve the current count from the database
+    connection.query('SELECT count FROM counters WHERE id = 1', function (error, results, fields) {
+      if (error) throw error;
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ count: results[0].count }),
+      };
+    });
   } else {
     return {
       statusCode: 405,
@@ -21,6 +42,5 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     };
   }
 };
-
 
 export { handler };
