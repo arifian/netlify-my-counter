83233459a395ff5af5660a41532f9d70f47a014f
Arifian <arifian.rahardianda@zeniuseducation.com>
2023-01-05 11:32:27 +0700

    local not running?

--- netlify/functions/identity-login.ts
+++ netlify/functions/identity-login.ts
@@ -1,12 +1,15 @@
+// why this function is not called by netlify dev
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 const handler: Handler = async function (event: HandlerEvent, context: HandlerContext) {
     const { identity, user } = context.clientContext;
+    console.log('logging');
+    console.log(identity);
     console.log(user);
+    console.log("hello " + user);
     // Do stuff and return a response...
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello, " + user }),
     };
 };
 
